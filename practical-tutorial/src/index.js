62c8f033fdce12b4c03063b72c542e0f0af98ad6
Samuel Gomez <samuel.gomez@valtech.com>
2021-02-20 13:55:12 +0100

    Practical tutorial: label 'move history list'

--- practical-tutorial/src/index.js
+++ practical-tutorial/src/index.js
@@ -116,6 +116,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{ status }</div>
+          <p>Move history list:</p>
           <ol>{ moves }</ol>
         </div>
       </div>

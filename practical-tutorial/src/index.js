2ecf143aa72cf1b1c5b6b16d76c117196177c630
Samuel Gomez <samuel.gomez@valtech.com>
2021-02-20 16:12:43 +0100

    Practical tutorial: Idea for improvement 3; WIP after one hour of hard mental work.

--- practical-tutorial/src/index.js
+++ practical-tutorial/src/index.js
@@ -20,24 +20,22 @@ class Board extends React.Component {
     );
   }
 
+  renderRow(j) {
+    const rowSquares = [0, 1, 2].map(i => this.renderSquare((j * 3) + i));
+
+    return (
+      <div className="board-row">
+        { rowSquares }
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.renderRow(0)}
+        {this.renderRow(1)}
+        {this.renderRow(2)}
       </div>
     );
   }

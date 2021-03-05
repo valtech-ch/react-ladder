e8fed13137a8c0593b1ee02b689a47fb3b1b2d40
Samuel Gomez <samuel.gomez@valtech.com>
2021-03-05 16:31:31 +0100

    Bulk of eslint fixes for index.tsx

--- practical-tutorial/src/index.tsx
+++ practical-tutorial/src/index.tsx
@@ -5,9 +5,9 @@ import './index.css';
 type SquareValue = string | null;
 
 function Square(props: {
-    highlighted: boolean,
-    onClick: () => void,
-    value: SquareValue,
+  highlighted: boolean,
+  onClick: () => void,
+  value: SquareValue,
 }) {
   const { highlighted, onClick, value } = props;
 
@@ -24,26 +24,30 @@ function Square(props: {
 }
 
 class Board extends React.Component<{
-    highlightedSquares: number[],
-    onClick: (i: number) => void,
-    squares: SquareValue[],
+  highlightedSquares: number[],
+  onClick: (i: number) => void,
+  squares: SquareValue[],
 }> {
   renderSquare(i: number, highlighted: boolean) {
+    const { onClick, squares } = this.props;
+
     return (
       <Square
         key={i}
-        value={this.props.squares[i]}
+        value={squares[i]}
         highlighted={highlighted}
-        onClick={() => this.props.onClick(i)}
+        onClick={() => onClick(i)}
       />
     );
   }
 
   renderRow(j: number) {
+    const { highlightedSquares } = this.props;
+
     const rowSquares = [0, 1, 2].map((i) => {
       const squareId = (j * 3) + i;
 
-      const highlighedSquare = this.props.highlightedSquares.includes(squareId);
+      const highlighedSquare = highlightedSquares.includes(squareId);
 
       return this.renderSquare(squareId, highlighedSquare);
     });

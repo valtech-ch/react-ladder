3af62b297333407a9ca9ec3cf545f35285b8251f
Samuel Gomez <samuel.gomez@valtech.com>
2021-05-07 19:38:30 +0200

    Make 'eslint .' useful if run from practical-tutorial.
    By ignoring the build folder and filling parserOptions.project as requested

--- practical-tutorial/.eslintrc.js
+++ practical-tutorial/.eslintrc.js
@@ -3,18 +3,23 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  ignorePatterns: [
+    "build"
+  ],
   extends: [
     '@valtech-ch/eslint-config/config/react',
     '@valtech-ch/eslint-config/rules/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    createDefaultProgram: true,
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
     sourceType: 'module',
     project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   plugins: [
     'react',
